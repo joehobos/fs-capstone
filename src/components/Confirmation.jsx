8dbddd3fe18d97eebb020053a143071cfe918a16
joehobos <joehobos@DESKTOP-A520OBQ>
2023-09-25 19:29:12 -0700

    update confirmation

--- src/components/Confirmation.jsx
+++ src/components/Confirmation.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 export default function Confirmation() {
-    console.log(Confirmation)
+    console.log('This is your Confirmation Reciept')
     return (
         <div>
             <p>Thank You for the purchase!</p>
